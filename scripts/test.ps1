e92c20d51112e7fc756c87ae614f59e223c1e38e
Friedrich von Never <friedrich@fornever.me>
2017-10-12 22:18:20 +0700

    #2: enable tests on Appveyor

---
+++ scripts/test.ps1
@@ -0,0 +1,29 @@
+param (
+    $Configuration = 'Release',
+
+    $Root = "$PSScriptRoot/..",
+    $TestScripts = "$Root/tests/scripts",
+    $malco = "$Root/build/$Configuration/malco.exe"
+)
+
+$ErrorActionPreference = 'Stop'
+
+$success = $true
+
+function Run-Test($test) {
+    Write-Output "[tests] Compiling test $($test.Name)..."
+    & $malco -c $test.FullName
+    echo $LASTEXITCODE
+    if ($LASTEXITCODE -eq 0) {
+        Write-Output "[tests] Test $($test.Name) success"
+    } else {
+        Write-Output "[tests] Test $($test.Name) error"
+        $script:success = $false
+    }
+}
+
+Get-ChildItem $TestScripts/*.rasm | ForEach-Object { Run-Test $_ }
+
+if (!$success) {
+    exit 1
+}
